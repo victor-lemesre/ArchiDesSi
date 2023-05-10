e015161a5d058dfb17da4967f9f2c39f754d1670
Victor Lemesre <victor.lemesre@gmail.com>
2023-05-10 11:20:56 +0200

    création temperatureBusinessImpl

---
+++ LocationApp/src/main/java/fr/univtours/polytech/locationapp/dao/TemperatureDAOImpl.java
@@ -0,0 +1,29 @@
+package fr.univtours.polytech.locationapp.dao;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import fr.univtours.polytech.locationapp.model.weathermap.WsWeatherMapResult;
+
+public class TemperatureDAOImpl implements TemperatureDAO {
+
+	private static String URL = "https://api-adresse.data.gouv.fr";
+	private static String key = "2832ed6df19fca6ad3e115a805becf6d";
+
+	@Override
+	public Double getTemperature(Double longitude, Double latitude) {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL);
+
+		target = target.queryParam("appid", key);
+		target = target.queryParam("lon", longitude);
+		target = target.queryParam("lat", latitude);
+
+
+		WsWeatherMapResult wsResult = target.request(MediaType.APPLICATION_JSON).get(WsWeatherMapResult.class);
+		return wsResult.getMain().getTemp();
+	}
+
+}
